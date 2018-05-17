ba5add87be37e77fc11c52a4534969fc0cd43c5c
KostiantynZ <integrity1@mail.ru>
2018-05-17 11:19:11 +0300

    chat progress

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Events\ChatEvent;
+use Illuminate\Support\Facades\Auth;
+
+class ChatController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function chat()
+    {
+        return view('chat');
+    }
+
+    /*public function send(request $request)
+    {
+        $user = User::find(Auth::id());
+        event(new ChatEvent($request->message, $user));
+    }*/
+
+    public function send()
+    {
+        $user = User::find(Auth::id());
+        event(new ChatEvent('hello', $user));
+    }
+}
---
+++ app/Http/Controllers/RecipesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RecipesController extends Controller
+{
+
+    public function meat()
+    {
+        return view('recipes.meat');
+    }
+}
