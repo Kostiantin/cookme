6fe584df984b3c74f48e1f7941405913f9e2e158
KostiantynZ <integrity1@mail.ru>
2018-06-24 15:34:38 +0300

    changes done

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,22 @@
 <?php
-use Illuminate\Support\Facades\Config;
-use App\Message;
 
 $localizer = \App::make(\Waavi\Translation\UriLocalizer::class);
 
 //localization of all routes
 Route::group(['prefix' => $localizer->localeFromRequest()], function(){
 
+    //home
     Route::get('/', 'IndexController@index');
-
+    // auth
     Auth::routes();
 
     //recipes pages
-    Route::get('/recipes/{theme}', 'RecipesController@recipes');
-
+    Route::get('/recipes/{theme}', 'RecipesController@recipes')->name('recipes');
 
     //chat routes
-    Route::get('/chat', 'ChatController@index')->middleware('auth');
-    Route::get('/messages', 'ChatController@messages')->middleware('auth');
+    Route::get('/chat', 'ChatController@index')->name('chat')->middleware('auth');
+    Route::get('/messages', 'ChatController@messages')->name('chat_messages')->middleware('auth');
     // send and save user message
-    Route::post('savemessage', 'ChatController@savemessage');
+    Route::post('savemessage', 'ChatController@savemessage')->name('chat_save_message')->middleware('auth');
 
 });
-
-
-
